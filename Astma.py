2dc961edf86e2374784813a0bf34dc053e330c3b
Olga Dmitriyeva <olgamac@Olgas-MacBook-Air.local>
2015-01-11 15:51:40 -0700

    Add 2 new files for Astma study

---
+++ Astma.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  8 13:42:20 2015
+
+@author: olga
+"""
+
+import csv
+import numpy as np
+import pydot, StringIO
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+
+from sklearn import tree
+from sklearn.svm import SVC
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn.cross_validation import train_test_split, KFold, cross_val_score
+from sklearn import metrics
+from sklearn import preprocessing
+
+from scipy.stats import sem
+######################
+
+def measure_performance(X,y,clf, show_accuracy = True, show_classification_report = True, show_confusion_matrix = True):
+    '''
+    Helper function to print the statistics:
+    takes the set of data and targets, runs a prediction, prints the stats
+    '''
+    y_pred = clf.predict(X)
+    if show_accuracy:
+        print "Accuracy:{0:.3f}".format(metrics.accuracy_score(y,y_pred)),"\n"
+    if show_classification_report:
+        print "Classification report"
+        print metrics.classification_report(y,y_pred),"\n"
+    if show_confusion_matrix:
+        print "Confusion matrix:"
+        print metrics.confusion_matrix(y,y_pred),"\n"
+#####################
+        
+data = np.genfromtxt('atemwege.tsv',delimiter=' ')
+data = np.delete(data,0,0)
+data = np.delete(data,0,1)
+
+
+#removing rows with "-1" value (nonexisting answer)
+b =[]
+for row in range(len(data)):
+    if -1 in data[row]:
+        b.append(row)
+
+data = np.delete(data,b, 0)
+
+#creating the column which is going to be a classifier and removing that column form numpy array
+
+data_y = data[:,21]
+data_X= np.delete(data,[21],1)
+data_X = np.delete(data_X,[8,9,10,13,14,15],1)
+#data_X = data_X[:,11]
+
+X_train, X_test, y_train, y_test = train_test_split(data_X, data_y, test_size = 0.25, random_state = 33)
+scaler = preprocessing.StandardScaler().fit(X_train)
+
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+colors = ["yellow", "red"]
+legend = ['healthy', 'sick']
+
+#for i in range(len(colors)):
+#    xs = data[:,16][y_train == i]
+#    ys = data[:,17][y_train == i]
+#    plt.scatter(xs,ys, c = colors[i])
+#plt.legend(legend)
+
+##################################
+
+#clf = SVC(C = 100000.0, kernel='rbf')
+#clf = SVC(kernel='linear')
+
+#clf = tree.DecisionTreeClassifier(criterion = 'entropy', max_depth = 7, min_samples_leaf = 5)
+
+#clf = KNeighborsClassifier(n_neighbors=10)
+
+clf = GradientBoostingClassifier(n_estimators=100, learning_rate=1, max_depth=1, random_state=5).fit(X_train, y_train)
+
+clf = clf.fit(X_train,y_train)
+print "score", clf.score(X_test, y_test)
+measure_performance(X_test, y_test,clf)
+###############################
+#dot_data = StringIO.StringIO()
+#tree.export_graphviz(clf, out_file = dot_data, feature_names = ['sex','age','first class','second class','third class'])
+#graph = pydot.graph_from_dot_data(dot_data.getvalue())
+#graph.write_png('titanic.png')
+
+##############################
+
+
+
