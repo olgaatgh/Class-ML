96636462a9b177ad08a5badb45330202ab86086c
Olga Dmitriyeva <olgamac@Olgas-MacBook-Air.local>
2015-02-05 18:15:13 -0700

    Yet another revision of Astma.py

--- Astma.py
+++ Astma.py
@@ -22,7 +22,7 @@ from sklearn.datasets import load_digits
 from sklearn.learning_curve import learning_curve
 from sklearn import tree
 from sklearn.svm import SVC
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import GradientBoostingClassifier,ExtraTreesClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import SGDClassifier
@@ -57,7 +57,9 @@ def loo_cv(X_train,y_train, clf):
     scores = np.zeros(X_train[:].shape[0])
     for train_index, test_index in loo:
         X_train_cv, X_test_cv = X_train[train_index], X_train[test_index]
+        
         y_train_cv, y_test_cv = y_train[train_index],y_train[test_index]
+        print y_train_cv
         clf = clf.fit(X_train_cv,y_train_cv)
         y_pred = clf.predict(X_test_cv)
         scores[test_index] = metrics.accuracy_score(y_test_cv.astype(int), y_pred.astype(int))
@@ -117,6 +119,7 @@ data_X= np.delete(data,[21],1)
 
 #remove some featurres and leaving only 15 of them:
 data_X = np.delete(data_X,[8,9,10,13,14,15],1)
+#data_X = np.delete(data_X,[8,9,10,13,14,15,11,12,15,17,18,19,20],1)
 
 #scaler = preprocessing.StandardScaler().fit(data_X)
 #
@@ -129,11 +132,11 @@ data_X = np.delete(data_X,[8,9,10,13,14,15],1)
 feature_names = ['polution zone','allergies', 'astma','mother smokes','father smokes','parental education','frequent colds','cough','height','sex','weight','lung cap','airflow','airflow 50%','airflow 75%']
 
 #t_size = 0.25
-#X_train, X_test, y_train, y_test = train_test_split(data_X, data_y, test_size = t_size, random_state = 33)
-#scaler = preprocessing.StandardScaler().fit(X_train)
-#
-#X_train = scaler.transform(X_train)
-#X_test = scaler.transform(X_test)
+X_train, X_test, y_train, y_test = train_test_split(data_X, data_y, test_size = 0.25, random_state = 33)
+scaler = preprocessing.StandardScaler().fit(X_train)
+
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 
 scaler = preprocessing.StandardScaler().fit(data_X)
 
@@ -152,15 +155,20 @@ legend = ['healthy', 'sick']
 ##################################
 
 #clf = SVC(C = 100000.0, kernel='rbf')
-#clf = SVC(kernel='linear')
+clf = SVC(kernel='linear')
 
 #clf = tree.DecisionTreeClassifier(criterion = 'entropy', max_depth = 5, min_samples_leaf = 33)
 #clf = clf.fit(X_train,y_train)
+##################################
+# To study the importance of the features:
+#clf = ExtraTreesClassifier(n_estimators = 250, random_state = 0)
+#clf = clf.fit(X_train,y_train)
 #importances = clf.feature_importances_
+#std = np.std([clf.feature_importances_ for clf in clf.estimators_],axis = 0)
 #indices = np.argsort(importances)[::-1]
 #print("Feature ranking:")
-#for f in range(10):
-#    print " %d. feature %d" % (f + 1, indices[f]), feature_names[f], importances[indices[f]]
+#for f in range(15):
+#    print " %d. feature %d" % (f + 1, indices[f]),feature_names[indices[f]],importances[indices[f]]
 
 #clf = KNeighborsClassifier(n_neighbors=5,  weights = 'distance')
 
@@ -175,47 +183,51 @@ legend = ['healthy', 'sick']
 #plt.show()
 
 #digits = load_digits()
-X, y = data_X, data_y
-
-
-title = "Learning Curves (Gradient Booster)"
-# Cross validation with 100 iterations to get smoother mean test and train
-# score curves, each time with 20% data randomly selected as a validation set.
-#cv = cross_validation.ShuffleSplit(digits.data.shape[0], n_iter=100,
-#                                   test_size=0.2, random_state=0)
-
-cv = cross_validation.ShuffleSplit(data_X.shape[0], n_iter=10,
-                                   test_size=0.2, random_state=0)
-
-estimator = GradientBoostingClassifier(n_estimators=100, learning_rate=1, max_depth=1, random_state=5).fit(X, y)
-plot_learning_curve(estimator, title, X, y, ylim=(0.5, 1.01), cv=cv, n_jobs=1)
-
-#title = "Learning Curves (SVM, RBF kernel, $\gamma=0.001$)"
-## SVC is more expensive so we do a lower number of CV iterations:
-#cv = cross_validation.ShuffleSplit(digits.data.shape[0], n_iter=10,
-#                                   test_size=0.2, random_state=0)
-#estimator = SVC(gamma=0.001)
-#plot_learning_curve(estimator, title, X, y, (0.7, 1.01), cv=cv, n_jobs=4)
+###########################
+#X, y = data_X, data_y
 #
-plt.show()
+#
+#title = "Learning Curves (Random Forest (10))"
+## Cross validation with 100 iterations to get smoother mean test and train
+## score curves, each time with 20% data randomly selected as a validation set.
+##cv = cross_validation.ShuffleSplit(digits.data.shape[0], n_iter=100,
+##                                   test_size=0.2, random_state=0)
+#
+#cv = cross_validation.ShuffleSplit(data_X.shape[0], n_iter=10,
+#                                   test_size=0.2, random_state=0)
+#
+#estimator = SVC(gamma =0.01)
+#X_train, X_test, y_train, y_test = train_test_split(data_X, data_y, test_size = 0.3, random_state = 33)
+#clf = estimator.fit(X_train, y_train)
+#measure_performance(X_test, y_test,clf)
+#plot_learning_curve(estimator, title, X, y, ylim=(0.5, 1.01), cv=cv, n_jobs=1)
+#
+##title = "Learning Curves (SVM, RBF kernel, $\gamma=0.001$)"
+### SVC is more expensive so we do a lower number of CV iterations:
+##cv = cross_validation.ShuffleSplit(digits.data.shape[0], n_iter=10,
+##                                   test_size=0.2, random_state=0)
+##estimator = SVC(gamma=0.001)
+##plot_learning_curve(estimator, title, X, y, (0.7, 1.01), cv=cv, n_jobs=4)
+##
+#plt.show()
+############################
 
-
-#clf = clf.fit(X_train,y_train)
+clf = clf.fit(X_train,y_train)
 #y_pred = clf.predict(X_test)
 #print "score", clf.score(X_test, y_test),"=/- {", mean_squared_error(y_test, y_pred),"}"
-#measure_performance(X_test, y_test,clf)
+measure_performance(X_test, y_test,clf)
 
 #print "Leave One Out"
 #loo_cv(X_train, y_train, clf)
 ###############################
-#K-fold cross validation
+###K-fold cross validation
 
 
-#cv = KFold(data_X.shape[0], 5, shuffle = True, random_state = 33)
+cv = KFold(data_X.shape[0], 5, shuffle = True, random_state = 33)
 
-#scores = cross_val_score(clf,data_X, data_y, cv=cv)
-#print "Cross-validation 5 fold"
-#print np.mean(scores),"(+/-",sem(scores),")"
+scores = cross_val_score(clf,data_X, data_y, cv=cv)
+print "Cross-validation 5 fold"
+print np.mean(scores),"(+/-",sem(scores),")"
 ###############################
 #dot_data = StringIO.StringIO()
 #tree.export_graphviz(clf, out_file = dot_data, feature_names = ['polution zone','allergies', 'astma','mother smokes','faather smokes','parental education','frequent colds','cough','height','sex','weight','lung cap','airflow','airflow 50%','airflow 75%'])
